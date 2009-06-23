c2ef93e90c68d9f089127aa54519dc112b9c8623
kawaiti <kawaiti@gmail.com>
2009-06-23 17:19:39 +0900

    role controllerの作成, 星画像の追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,27 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  #== 星を表示するヘルパーメソッド 
+  #
+  #==== 引数
+  #* :number => integer : 星の数（デフォルト値：1）
+  #* :weight => intefer : 大きい星の重み（デフォルト値：10）
+  #* :big_star => true | false : 大きい星を使う（デフォルト値：false）
+  #
+  #==== 戻り値
+  #* image_tag
+  #
+  def show_stars(options = {})
+    result = "" 
+    number = options[:number] || 1
+    weight = options[:weight] || 10
+    big_star = options[:big_star] || false
+  
+    if big_star
+      result = image_tag("star1.gif", :size => "30x30", :mouseover => "star2.gif") * (number / weight)
+      result << image_tag("star1.gif", :size => "20x20", :mouseover => "star2.gif") * (number % weight)
+    else
+      result = image_tag("star1.gif", :size => "20x20", :mouseover => "star2.gif") * number
+    end
+    return result
+  end
 end
